bc90c25c286cdcdb01d2baed1caba51e9eb30cb4
ifeanyiripple <ifeanyi.ripple@gmail.com>
2026-06-16 15:02:06 +0100

    h

--- src/app/products.xml/route.ts
+++ src/app/products.xml/route.ts
@@ -28,8 +28,7 @@ export async function GET() {
     },
   })
 
-  const xml = `
-<?xml version="1.0" encoding="UTF-8" ?>
+  const xml = `<?xml version="1.0" encoding="UTF-8" ?>
 <rss 
   version="2.0"
   xmlns:g="http://base.google.com/ns/1.0"
